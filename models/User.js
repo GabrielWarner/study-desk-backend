db841957512f826b2bbbd84a8cded57f7bf41d5c
Jiajie <jiajiehe@yahoo.com>
2022-08-26 21:17:25 -0700

    added mongo user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,33 @@
+const mongoose = require('mongoose'),
+  bcrypt = require('bcrypt'),
+  Schema = mongoose.Schema;
 
+/**
+ * User Schema
+ */
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true
+  },
+  hash_password: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.hash_password);
+};
+
+mongoose.model('User', UserSchema);
