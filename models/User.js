9aa4996b7d1ee11beb9ba1c5053f83bb2a92c0f0
Jiajie <jiajiehe@yahoo.com>
2022-08-30 12:26:03 -0700

    update email requirement

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+      match: [/^([a-zA-Z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
     },
     password: {
       type: String,
